5a49009410df75155fbdaa1c68a187e451b89919
Shahroz Ali <161182286+shahroz-09ali@users.noreply.github.com>
2025-07-14 22:46:46 +0530

    Create Sample_Of_data.sql

---
+++ Sample_Of_data.sql
@@ -0,0 +1,12 @@
+-- all command to backup and recovery
+create database sample;
+use sample;
+create table students( roll_no int, name varchar(34), email varchar(30));
+insert into students values( 1 ,"shahroz", "shahrozali09@gmail.com");
+insert into students values( 2 ,"Aniket", "aniketkumar09@gmail.com");
+insert into students values( 3 ,"charu", "charujoshi09@gmail.com");
+insert into students values( 4 ,"kapil", "kapilsharma09@gmail.com");
+insert into students values( 5 ,"lovecesh", "lovekesh09@gmail.com");
+insert into students values( 6 ,"aman", "amanrathore98@gmail.com");
+select *from students;
+
